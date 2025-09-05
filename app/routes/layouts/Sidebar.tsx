7f88c28b38171fdfe5f9a8ab86bfdccdc2e2404c
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-09-06 00:39:32 +0530

    Update Sidebar.tsx

--- app/routes/layouts/Sidebar.tsx
+++ app/routes/layouts/Sidebar.tsx
@@ -20,7 +20,7 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-13 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
@@ -50,7 +50,7 @@ export default function Page() {
           </div>
           <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div> */}
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="w-full min-h-[] items-center justify-center flex-col gap-4 p-4 pt-0">
           <Outlet />
         </div>
       </SidebarInset>
