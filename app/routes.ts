4068c00068fcdb21c9421a7cac6cd69e035a0718
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-09-06 00:39:25 +0530

    protected routes

--- app/routes.ts
+++ app/routes.ts
@@ -6,22 +6,25 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  layout("routes/layouts/Sidebar.tsx", [
-    route("/admin", "routes/AdminDashboard.tsx"),
-    route("/slider", "routes/slider/TheSlider.tsx"),
-    route("/brands", "routes/brands/TheBrands.tsx"),
-    route("/advertistment", "routes/advertistment/TheAdvertistment.tsx"),
-    route(
-      "/delivery-location",
-      "routes/delivery-location/TheDeliveryLocation.tsx"
-    ),
-    route("/payment-types", "routes/payment-types/ThePaymentTypes.tsx"),
-    route("/coupon-voucher", "routes/coupon-voucher/TheCouponVoucher.tsx"),
-    route("/products", "routes/products/TheProduct.tsx"),
-    route("/promotion", "routes/promotion/ThePromotion.tsx"),
-    route("/dotd", "routes/deal-of-the-day/TheDealOfTheDay.tsx"),
-    route("/blog", "routes/blog/TheBlog.tsx"),
-    route("/faq", "routes/faq/TheFAQ.tsx"),
+  index("routes/Login.tsx"),
+  route("/register", "routes/Register.tsx"),
+  layout("routes/requireAuth.tsx", [
+    layout("routes/layouts/Sidebar.tsx", [
+      route("/admin", "routes/AdminDashboard.tsx"),
+      route("/slider", "routes/slider/TheSlider.tsx"),
+      route("/brands", "routes/brands/TheBrands.tsx"),
+      route("/advertistment", "routes/advertistment/TheAdvertistment.tsx"),
+      route(
+        "/delivery-location",
+        "routes/delivery-location/TheDeliveryLocation.tsx"
+      ),
+      route("/payment-types", "routes/payment-types/ThePaymentTypes.tsx"),
+      route("/coupon-voucher", "routes/coupon-voucher/TheCouponVoucher.tsx"),
+      route("/products", "routes/products/TheProduct.tsx"),
+      route("/promotion", "routes/promotion/ThePromotion.tsx"),
+      route("/dotd", "routes/deal-of-the-day/TheDealOfTheDay.tsx"),
+      route("/blog", "routes/blog/TheBlog.tsx"),
+      route("/faq", "routes/faq/TheFAQ.tsx"),
+    ]),
   ]),
 ] satisfies RouteConfig;
