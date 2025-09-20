a8abf11b4e0e588f7bb39a526efbb5771aad284c
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-09-20 23:11:22 +0530

    Update RequireAuth.tsx

--- app/routes/RequireAuth.tsx
+++ app/routes/RequireAuth.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
 import { Outlet, Navigate, useLocation } from "react-router";
 import useAuthStore from "~/stores/useAuthStore";
 
 const RequireAuth = () => {
   //   const auth = useAuthStore();
   const location = useLocation();
-  const user = useAuthStore((state) => state.fetchUser);
+  const fetchUser = useAuthStore((state) => state.fetchUser);
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-  console.log(user());
+  const [loginStatus, setLoginStatus] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let user = await fetchUser();
+      user && setLoginStatus(true);
+      console.log(user);
+      if (!user) {
+        setTimeout(() => fetchUser(), 200);
+        console.log(user);
+        setLoginStatus(true);
+      }
+    })();
+  }, []);
+
+  console.log(loginStatus);
+  console.log(isLoggedIn);
+
+  const userLoggedIn = localStorage.getItem("userLoggedIn");
 
   //   const userState = useAuthStore((state) => state.isLoggedIn);
 
   //   console.log("User: ", userState);
 
-  if (!isLoggedIn) {
-    return <Navigate to="/" state={{ from: location }} replace />;
+  if (userLoggedIn) {
+    return <Outlet />;
   }
-  return <Outlet />;
+
+  if (!isLoggedIn) {
+    return <Navigate to="/" replace />;
+  } else {
+    return <Outlet />;
+  }
 };
 
 export default RequireAuth;
