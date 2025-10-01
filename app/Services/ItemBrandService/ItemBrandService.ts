54debe26b03c55a728ddfe72bdfcccac37419a80
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:49:03 +0530

    Update ItemBrandService.ts

--- app/Services/ItemBrandService/ItemBrandService.ts
+++ app/Services/ItemBrandService/ItemBrandService.ts
@@ -2,16 +2,48 @@ import axiosInstance from "~/lib/axios";
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 class ItemBrandService {
-  getAll = async () => {
+  getAll = async (cursor: string | null = null) => {
     try {
       const response = await axiosInstance({
         method: "get",
-        url: `${API_URL}item-brand`,
+        url: cursor
+          ? `${API_URL}item-brand?cursor=${cursor}`
+          : `${API_URL}item-brand`,
       });
       return response.data.brands;
     } catch (e) {
       const error = e instanceof Error ? e.message : String(e);
       return error;
+    }
+  };
+
+  create = async (data: any) => {
+    try {
+      const response = await axiosInstance({
+        method: "post",
+        url: `${API_URL}item-brand`,
+        data: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.data.brand;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : String(e);
+      throw error;
+    }
+  };
+
+  search = async (search: string | null = null) => {
+    try {
+      const response = await axiosInstance({
+        method: "get",
+        url: `${API_URL}item-brand?search=${search}`,
+      });
+      return response.data?.brands;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : String(e);
+      throw error;
     }
   };
 }
