c215c5fd4334487478beb7fe56d03cd9f2f9eed2
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:49:06 +0530

    Update ItemCategoryService.ts

--- app/Services/ItemCategoryService/ItemCategoryService.ts
+++ app/Services/ItemCategoryService/ItemCategoryService.ts
@@ -2,16 +2,48 @@ import axiosInstance from "~/lib/axios";
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 class ItemCategoryService {
-  getAll = async () => {
+  getAll = async (cursor: string | null = null) => {
     try {
       const response = await axiosInstance({
         method: "get",
-        url: `${API_URL}item-category`,
+        url: cursor
+          ? `${API_URL}item-category?cursor=${cursor}`
+          : `${API_URL}item-category`,
       });
       return response.data.categories;
     } catch (e) {
       const error = e instanceof Error ? e.message : String(e);
       return error;
+    }
+  };
+
+  create = async (data: any) => {
+    try {
+      const response = await axiosInstance({
+        method: "post",
+        url: `${API_URL}item-category`,
+        data: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.data.category;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : String(e);
+      throw error;
+    }
+  };
+
+  search = async (search: string | null = null) => {
+    try {
+      const response = await axiosInstance({
+        method: "get",
+        url: `${API_URL}item-category?search=${search}`,
+      });
+      return response.data?.categories;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : String(e);
+      throw error;
     }
   };
 }
