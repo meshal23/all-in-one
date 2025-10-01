917db437c0f77ba677d36d16c60d775cbed45e55
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:47:03 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Spinner } from "components/ui/shadcn-io/spinner";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -43,6 +44,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function HydrateFallback() {
+  return <Spinner color="black" />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
