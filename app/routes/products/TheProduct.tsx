dec7d3cd04d88617b135fb85f456ed74d1259d5c
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-10-01 20:48:17 +0530

    Update TheProduct.tsx

--- app/routes/products/TheProduct.tsx
+++ app/routes/products/TheProduct.tsx
@@ -1,15 +1,8 @@
 import { ImagePlus, MoreHorizontal } from "lucide-react";
 import React from "react";
 import Searchbar from "~/components/main/Searchbar";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
-import { Outlet, useNavigate } from "react-router";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Await, Outlet, useNavigate, useSearchParams } from "react-router";
 import { DataTable } from "~/components/FormComponents/DataTable";
 import type { Route } from "./+types/TheProduct";
 import ItemMasterService from "~/Services/ItemMasterService/ItemMasterService";
@@ -26,10 +19,22 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { Button } from "~/components/ui/button";
+import TableSkeleton from "~/components/main/TableSkeleton";
 
-export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  const response = await ItemMasterService.getAll();
-  return response;
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  const url = new URL(request.url);
+  const cursor = url.searchParams.get("cursor");
+  const search = url.searchParams.get("search");
+
+  let data = null;
+
+  if (search == null || search == "") {
+    data = await ItemMasterService.getAll(cursor);
+  } else if (search.length == 3) {
+    data = await ItemMasterService.search(search);
+  }
+
+  return { data };
 }
 
 const columns: ColumnDef<ShowProductType>[] = [
@@ -122,9 +127,28 @@ const columns: ColumnDef<ShowProductType>[] = [
 ];
 
 const TheProduct = ({ loaderData }: Route.ComponentProps) => {
-  const data = loaderData;
-  console.log(data);
+  const isSearchValueEmpty =
+    new URLSearchParams(document.location.search).get("search") !== "";
+
+  const isSearchValueExist =
+    new URLSearchParams(document.location.search).get("search") !== null;
+
+  const { data } = isSearchValueEmpty
+    ? isSearchValueExist
+      ? loaderData
+      : loaderData?.data
+    : loaderData?.data;
+  const nextPageUrl = loaderData.data?.next_page_url;
+  const nextCursor = loaderData.data?.next_cursor;
+  const prevPageUrl = loaderData.data?.prev_page_url;
+  const prevCursor = loaderData.data?.prev_cursor;
+
+  console.log(nextPageUrl);
+
   const navigate = useNavigate();
+
+  const [search, setSearch] = React.useState("");
+  console.log(search);
 
   const tableData: ShowProductType[] = data?.map((item: ShowProductType) => {
     return {
@@ -147,148 +171,31 @@ const TheProduct = ({ loaderData }: Route.ComponentProps) => {
               className="px-5 py-2 text-base font-medium text-center inline-flex gap-1 items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <ImagePlus />
-              <span>Create Slider</span>
+              <span>Create Item</span>
             </button>
           </DialogTrigger>
           <DialogContent className="min-w-2/4">
-            {/* <DialogHeader>
-              <DialogTitle>Are you absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </DialogDescription>
-            </DialogHeader> */}
             <Outlet />
           </DialogContent>
         </Dialog>
       </div>
-      {/* <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Google Pixel Phone
-              </th>
-              <td className="px-6 py-4">Gray</td>
-              <td className="px-6 py-4">Phone</td>
-              <td className="px-6 py-4">$799</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple Watch 5
-              </th>
-              <td className="px-6 py-4">Red</td>
-              <td className="px-6 py-4">Wearables</td>
-              <td className="px-6 py-4">$999</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div> */}
-      {/* <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4"> */}
-      <DataTable
-        data={tableData}
-        columns={columns}
-        searchPlaceholder="Search By Item..."
-        searchColumn="name"
-      />
+      <React.Suspense fallback={<TableSkeleton />}>
+        <Await resolve={tableData}>
+          <DataTable
+            data={tableData}
+            columns={columns}
+            searchPlaceholder="Search By Item..."
+            searchColumn="name"
+            nextPageUrl={nextPageUrl}
+            nextCursor={nextCursor}
+            prevPageUrl={prevPageUrl}
+            prevCursor={prevCursor}
+            search={search}
+            setSearch={setSearch}
+          />
+        </Await>
+      </React.Suspense>
+
       {/* </div> */}
     </section>
   );
